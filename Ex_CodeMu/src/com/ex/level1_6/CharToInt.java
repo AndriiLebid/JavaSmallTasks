662a2f2e89a608417150c55f287f8a4df4486da6
Andrii Lebid <lebedav@gmail.com>
2024-08-19 18:46:40 -0300

    modified level 1_6

--- Ex_CodeMu/src/com/ex/level1_6/CharToInt.java
+++ Ex_CodeMu/src/com/ex/level1_6/CharToInt.java
@@ -9,11 +9,11 @@ public static void main(String args[]) {
 	char chr2 = '2';
 	char chr3 = '3';
 	
-	System.out.print(CharToInt(chr1)+CharToInt(chr2)+CharToInt(chr3));
+	System.out.print(CharToIntMethod(chr1)+CharToIntMethod(chr2)+CharToIntMethod(chr3));
 
 	}
 
-	private static int CharToInt(char ch) {
+	private static int CharToIntMethod(char ch) {
 		
 		return  ch - '0';
 
